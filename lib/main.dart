f31ad7e07501e255400208e3a55a8c218a70d90d
tgrangeo <thomas.grangeon9@gmail.com>
2023-01-10 01:17:03 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:bingo/view/gameScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: const MyHomePage(title: 'Bingo App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            OutlinedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => GameScreen(),
+                    ),
+                  );
+                },
+                child: const Text("play"))
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/view/gameScreen.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+
+class GameScreen extends StatefulWidget {
+  GameScreen({super.key});
+
+  @override
+  State<GameScreen> createState() => _GameScreen();
+}
+
+class _GameScreen extends State<GameScreen> {
+  List<int> selectedIndex = [];
+
+  List<int> bingo = [];
+  Color blue = Color.fromARGB(255, 140, 198, 213);
+  Color green = Colors.green;
+  bool verif = false;
+  Icon iconVerif = const Icon(Icons.verified_outlined, size: 100);
+
+  void reset() {
+    bingo.clear();
+    selectedIndex.clear();
+    verif = false;
+    setState(() {});
+  }
+
+  void checkBingo(context) {
+    if (bingo.length >= 5) {
+      showDialog(
+          context: context,
+          builder: ((context) => Dialog(
+                  child: Container(
+                  width: 600,
+                  height: 200,
+                child: Column(
+                  children: [
+                  const Text(
+                    "BINGO",
+                    style: TextStyle(fontSize: 100),
+                  ),
+                  OutlinedButton(
+                      onPressed: () {
+                        reset();
+                        Navigator.pop(context);
+                      },
+                      child: const Text("close"))
+                ]),
+              ))));
+    }
+  }
+
+  void setButton(index, context) {
+    if (selectedIndex.contains(index)) {
+      if (verif == true) {
+        bingo.add(index);
+        checkBingo(context);
+        print(bingo);
+      } else {
+        selectedIndex.remove(index);
+      }
+    } else if (verif == false) {
+      selectedIndex.add(index);
+    }
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+        body: Container(
+            color: Color.fromARGB(255, 66, 66, 66),
+            alignment: Alignment.center,
+            child: Row(
+              children: [
+                Container(
+                    // size de la grille
+                    width: screenWidth * 0.8,
+                    child: GridView(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 11,
+                        mainAxisExtent: screenHeight * 0.1,
+                        mainAxisSpacing: 10,
+                        crossAxisSpacing: 10,
+                      ),
+                      children: List<Widget>.generate(99, (int i) {
+                        return Builder(builder: (BuildContext context) {
+                          return ElevatedButton(
+                              onPressed: () {
+                                setButton(i, context);
+                              },
+                              style: OutlinedButton.styleFrom(
+                                backgroundColor: selectedIndex.contains(i)
+                                    ? verif
+                                        ? bingo.contains(i)
+                                            ? Colors.red
+                                            : green
+                                        : green
+                                    : blue,
+                                shadowColor: Colors.black,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(500),
+                                  // side: BorderSide(color: Color.fromARGB(255, 89, 99, 244))
+                                ),
+                              ),
+                              child: Text('${i + 1}',
+                                  style: const TextStyle(fontSize: 48)));
+                        });
+                      }),
+                    )),
+                OutlinedButton(
+                  onPressed: () {
+                    setState(() {
+                      if (verif == false) {
+                        verif = true;
+                        iconVerif = const Icon(Icons.verified, size: 100);
+                      } else {
+                        iconVerif =
+                            const Icon(Icons.verified_outlined, size: 100);
+                        verif = false;
+                      }
+                      print(verif);
+                    });
+                  },
+                  child: iconVerif,
+                ),
+                OutlinedButton(
+                    onPressed: () {
+                      reset();
+                    },
+                    child: const Text("reset"))
+              ],
+            )));
+  }
+}
