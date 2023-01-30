d8468a7434e356b85f6ff3ba64b1ca5cf329837b
tgrangeo <thomas.grangeon9@gmail.com>
2023-01-30 12:29:21 +0100

    pause_screen_first_try

--- lib/view/pauseScreen.dart
+++ lib/view/pauseScreen.dart
@@ -2,6 +2,12 @@
 import 'package:flutter/material.dart';
 import '../widget/stopwatchtest.dart';
 
+List imgList = [
+  AssetImage('assets/sponsor/captain.png'),
+  AssetImage('assets/sponsor/jetm.png'),
+  AssetImage('assets/sponsor/justin_bridou.png'),
+  AssetImage('assets/sponsor/ricard.png'),
+];
 
 class PauseScreen extends StatefulWidget {
   PauseScreen({super.key});
@@ -18,11 +24,39 @@ class _PauseScreen extends State<PauseScreen> {
 
     return Scaffold(
         body: Container(
-      child: Column(
+            child: Column(children: [
+      SizedBox(height: screenHeight * 0.15),
+      Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+        const Image(
+          image: AssetImage('assets/ol.png'),
+          fit: BoxFit.cover,
+          width: 170,
+        ),
+        SizedBox(width: screenWidth * 0.05),
+        StopWatchTimerPage(),
+        SizedBox(width: screenWidth * 0.05),
+        const Image(
+          image: AssetImage('assets/ol.png'),
+          fit: BoxFit.cover,
+          width: 170,
+        ),
+      ]),
+      Expanded(child:ListView(
+        scrollDirection: Axis.horizontal,
+        shrinkWrap: true,
         children: [
-          StopWatchTimerPage(),
+          // showing list of images
+          for (var item in imgList)
+            Center(
+              child: Image(
+                image: item,
+                fit: BoxFit.cover,
+                width: 300,
+              ),
+              //child: Container(width: 150, height: 100, child: item),
+            )
         ],
       ),
-    ));
+    )])));
   }
 }
