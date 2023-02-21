0cc4597c329e3105bed0ab3a65c3c426ed0c0802
tgrangeo <thomas.grangeon9@gmail.com>
2023-02-21 14:45:29 +0100

    timer start on pause

--- lib/view/pauseScreen.dart
+++ lib/view/pauseScreen.dart
@@ -1,6 +1,6 @@
 //import 'package:flutter_animated_button/flutter_animated_button.dart';
 import 'package:flutter/material.dart';
-import '../widget/stopwatchtest.dart';
+import '../widget/stopwatch.dart';
 
 List imgList = [
   AssetImage('assets/sponsor/captain.png'),
@@ -25,38 +25,45 @@ class _PauseScreen extends State<PauseScreen> {
     return Scaffold(
         body: Container(
             child: Column(children: [
+      OutlinedButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: const Text("retour")),
       SizedBox(height: screenHeight * 0.15),
       Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-        const Image(
-          image: AssetImage('assets/ol.png'),
-          fit: BoxFit.cover,
-          width: 170,
-        ),
+        // const Image(
+        //   image: AssetImage('assets/ol.png'),
+        //   fit: BoxFit.cover,
+        //   width: 170,
+        // ),
         SizedBox(width: screenWidth * 0.05),
         StopWatchTimerPage(),
         SizedBox(width: screenWidth * 0.05),
-        const Image(
-          image: AssetImage('assets/ol.png'),
-          fit: BoxFit.cover,
-          width: 170,
-        ),
+        // const Image(
+        //   image: AssetImage('assets/ol.png'),
+        //   fit: BoxFit.cover,
+        //   width: 170,
+        // ),
       ]),
-      Expanded(child:ListView(
-        scrollDirection: Axis.horizontal,
-        shrinkWrap: true,
-        children: [
-          // showing list of images
-          for (var item in imgList)
-            Center(
-              child: Image(
-                image: item,
-                fit: BoxFit.cover,
-                width: 300,
-              ),
-              //child: Container(width: 150, height: 100, child: item),
-            )
-        ],
-      ),
-    )])));
+      Expanded(
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          shrinkWrap: true,
+          children: [
+            // showing list of images
+            for (var item in imgList)
+              Center(
+                child: Image(
+                  image: item,
+                  fit: BoxFit.cover,
+                  width: 300,
+                ),
+                //child: Container(width: 150, height: 100, child: item),
+              )
+          ],
+        ),
+      )
+    ])));
   }
 }
--- lib/widget/stopwatch.dart
+++ lib/widget/stopwatch.dart
@@ -12,77 +12,74 @@ class _StopWatchTimerPageState extends State<StopWatchTimerPage> {
   Duration duration = const Duration();
   Timer? timer;
 
-  bool countDown =true;
+  bool countDown = true;
 
   @override
   void initState() {
     super.initState();
     reset();
+    startTimer();
   }
 
-  void reset(){
-    if (countDown){
-      setState(() =>
-        duration = countdownDuration);
+  void reset() {
+    if (countDown) {
+      setState(() => duration = countdownDuration);
     } else {
-      setState(() =>
-        duration = const Duration());
+      setState(() => duration = const Duration());
     }
   }
 
-  void startTimer(){
-    timer = Timer.periodic(const Duration(seconds: 1),(_) => addTime());
+  void startTimer() {
+    timer = Timer.periodic(const Duration(seconds: 1), (_) => addTime());
   }
 
-  void addTime(){
+  void addTime() {
     final addSeconds = countDown ? -1 : 1;
     setState(() {
       final seconds = duration.inSeconds + addSeconds;
-      if (seconds < 0){
+      if (seconds < 0) {
         timer?.cancel();
-      } else{
+      } else {
         duration = Duration(seconds: seconds);
-
       }
     });
   }
 
-  void stopTimer({bool resets = true}){
-    if (resets){
-      reset();
-    }
-    setState(() => timer?.cancel());
-  }
+  // void stopTimer({bool resets = true}) {
+  //   if (resets) {
+  //     reset();
+  //   }
+  //   setState(() => timer?.cancel());
+  // }
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-    body: Center(
-      child: Column(
+  Widget build(BuildContext context) => Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           buildTime(),
-          const SizedBox(height: 80,),
-          buildButtons()
+          // const SizedBox(
+          //   height: 80,
+          // ),
+          // buildButtons()
         ],
-      ),
-    ),
-  );
+      );
 
-  Widget buildTime(){
-    String twoDigits(int n) => n.toString().padLeft(2,'0');
-    final hours =twoDigits(duration.inHours);
-    final minutes =twoDigits(duration.inMinutes.remainder(60));
-    final seconds =twoDigits(duration.inSeconds.remainder(60));
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        buildTimeCard(time: hours, header:'HOURS'),
-        const SizedBox(width: 8,),
-       buildTimeCard(time: minutes, header:'MINUTES'),
-       const SizedBox(width: 8,),
-       buildTimeCard(time: seconds, header:'SECONDS'),
-    ]
-    );
+  Widget buildTime() {
+    String twoDigits(int n) => n.toString().padLeft(2, '0');
+    final hours = twoDigits(duration.inHours);
+    final minutes = twoDigits(duration.inMinutes.remainder(60));
+    final seconds = twoDigits(duration.inSeconds.remainder(60));
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      buildTimeCard(time: hours, header: 'HEURES'),
+      const SizedBox(
+        width: 8,
+      ),
+      buildTimeCard(time: minutes, header: 'MINUTES'),
+      const SizedBox(
+        width: 8,
+      ),
+      buildTimeCard(time: seconds, header: 'SECONDES'),
+    ]);
   }
 
   Widget buildTimeCard({required String time, required String header}) =>
@@ -90,48 +87,51 @@ class _StopWatchTimerPageState extends State<StopWatchTimerPage> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Container(
-            padding: EdgeInsets.all(8),
+            padding: EdgeInsets.all(10),
             decoration: BoxDecoration(
-              color: Colors.grey[300],
-              borderRadius: BorderRadius.circular(20)
-            ),
+                color: Colors.grey[300],
+                borderRadius: BorderRadius.circular(20)),
             child: Text(
-            time, style: const TextStyle(fontWeight: FontWeight.bold,
-            color: Colors.black,fontSize: 50),),
+              time,
+              style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                  fontSize: 180),
+            ),
           ),
-          const SizedBox(height: 24,),
-          Text(header,style: const TextStyle(color: Colors.black45)),
+          const SizedBox(
+            height: 24,
+          ),
+          Text(header, style: const TextStyle(color: Colors.black45)),
         ],
       );
 
-  Widget buildButtons(){
-    final isRunning = timer == null? false: timer!.isActive;
-    final isCompleted = duration.inSeconds == 0;
-     return isRunning || isCompleted
-         ? Row(
-       mainAxisAlignment: MainAxisAlignment.center,
-       children: [
-         ButtonWidget(
-             text:'STOP',
-             onClicked: (){
-               if (isRunning){
-                 stopTimer(resets: false);
-               }
-             }),
-         const SizedBox(width: 12,),
-         ButtonWidget(
-             text: "CANCEL",
-             onClicked: stopTimer
-         ),
-       ],
-     )
-         : ButtonWidget(
-         text: "Start Timer!",
-         color: Colors.black,
-         backgroundColor: Colors.white,
-         onClicked: (){
-           startTimer();
-         });
-
-  }
+  // Widget buildButtons() {
+  //   final isRunning = timer == null ? false : timer!.isActive;
+  //   final isCompleted = duration.inSeconds == 0;
+  //   return isRunning || isCompleted
+  //       ? Row(
+  //           mainAxisAlignment: MainAxisAlignment.center,
+  //           children: [
+  //             ButtonWidget(
+  //                 text: 'STOP',
+  //                 onClicked: () {
+  //                   if (isRunning) {
+  //                     stopTimer(resets: false);
+  //                   }
+  //                 }),
+  //             const SizedBox(
+  //               width: 12,
+  //             ),
+  //             ButtonWidget(text: "CANCEL", onClicked: stopTimer),
+  //           ],
+  //         )
+  //       : ButtonWidget(
+  //           text: "Start Timer!",
+  //           color: Colors.black,
+  //           backgroundColor: Colors.white,
+  //           onClicked: () {
+  //             startTimer();
+  //           });
+  // }
 }
